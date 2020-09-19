1a7b65f095e704a765e08840d2e2204b76fba812
Ramji R <Ramji.R@emishealth.com>
2020-09-19 19:01:55 +0530

    Created a Duck Adapter

---
+++ AdapterPattern/DuckAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterPattern
+{
+	public class DuckAdapter : ITurkey
+	{
+		private readonly IDuck _duck;
+		public DuckAdapter(IDuck duck)
+		{
+			_duck = duck;
+		}
+
+
+		public void Fly()
+		{
+			_duck.Fly();
+		}
+
+		public void Gobble()
+		{
+			_duck.Quack();
+		}
+
+	}
+}
--- AdapterPattern/Program.cs
+++ AdapterPattern/Program.cs
@@ -8,15 +8,27 @@ namespace AdapterPattern
         {
             var turkey = new WildTurkey();
             var duck = new MallardDuck();
-            var adapter = new TurkeyAdapter(duck);
-
+            var adapter = new TurkeyAdapter(turkey);
+            var duckadapter = new DuckAdapter(duck);
             Tester(adapter);
+            Tester(duckadapter);
         }
 
         private static void Tester(IDuck duck)
         {
             duck.Fly();
             duck.Quack();
+			Console.WriteLine("Here the method called is Quack that belongs to Duck \n" );
+
+        }
+
+
+        private static void Tester(ITurkey turkey)
+        {
+            Console.WriteLine("Here the method called is Gobble that belongs to Turkey \n");
+
+            turkey.Fly();
+            turkey.Gobble();
         }
     }
 }
